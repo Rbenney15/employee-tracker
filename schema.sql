9c2c5a7ee2327add146d5e84d5cfbac491e885ef
Rob Benney <rob.benney@gmail.com>
2022-05-14 11:44:49 -0400

    employee role seeds and department seeds

--- schema.sql
+++ schema.sql
@@ -20,11 +20,25 @@ CREATE TABLE role (
 );
 
 -- Employee Role Table --
-CREATE TABLE role (
-
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 -- Department Seeds --
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
 
 -- Employee Role Seeds --
 
